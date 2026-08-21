ba56477bbe4543898d32cea1fa463fe2ca5e8d99
elingenierodeabordo-droid <elingenierodeabordo@gmail.com>
2026-08-21 18:11:09 +0200

    Update WoodOs.py

--- WoodOs.py
+++ WoodOs.py
@@ -391,7 +391,7 @@ ROJO = "\033[31m"
 # ===================================== SETUP ======================================================
 sistema = platform.system()
 devmode = cargar_devmode()
-ver = "1.50 beta"
+ver = "1.50"
 ajustpass = cargar_passmode()
 novedades = "Prueba el nuevo sistema de red y bluetooth! 🌐"
 os.system("clear")
