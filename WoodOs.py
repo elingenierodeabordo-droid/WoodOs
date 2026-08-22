707e7589619d41537699ef02c84c357c5c8760e7
elingenierodeabordo-droid <elingenierodeabordo@gmail.com>
2026-08-22 20:54:46 +0200

    Update WoodOs.py to 1.68

--- WoodOs.py
+++ WoodOs.py
@@ -458,7 +458,7 @@ ROJO = "\033[31m"
 # ===================================== SETUP ======================================================
 sistema = platform.system()
 devmode = cargar_devmode()
-ver = "1.66"
+ver = "1.68"
 ajustpass = cargar_passmode()
 novedades = "Prueba el nuevo sistema de red y bluetooth! 🌐"
 os.system("clear")
